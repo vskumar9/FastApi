d99211d593e95a9c2e11a487c8812a38f7a9b5ad
sanjeevkumar.v <sanjeevkumar.v@payoda.com>
2025-05-28 14:16:48 +0530

    Refactor delete_blog to be asynchronous and add update_blog functionality with error handling for non-existent blogs

--- blog/main.py
+++ blog/main.py
@@ -25,7 +25,7 @@ async def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return blog
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id: int, db: Session = Depends(get_db)):
+async def delete_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     # db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     if not blog:
@@ -34,6 +34,16 @@ def delete_blog(id: int, db: Session = Depends(get_db)):
     db.commit()
     return {'detail': 'Blog deleted'}
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+async def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} does not exist")
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    db.refresh(blog)
+    return {'detail': 'Blog updated'}
 
 @app.get('/blog')
 async def read_blog(db: Session = Depends(get_db)):
